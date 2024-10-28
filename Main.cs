21ca1d2e5558d2ddf9ef1d610dc54210af108269
kensho-pilkey <kpilkey@unc.edu>
2024-10-28 15:21:18 -0400

    Make basic main menu and placeholder gamescene

--- Main.cs
+++ Main.cs
@@ -1,12 +1,14 @@
 using Godot;
 using System;
 
-public partial class Label : Godot.Label
+public partial class Main : Node2D
 {
 	// Called when the node enters the scene tree for the first time.
+	public void _on_start_pressed() {
+		GetTree().ChangeSceneToFile("res://game_scene.tscn");
+	}
 	public override void _Ready()
 	{
-		Text = "Hello World";
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
