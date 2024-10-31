cfb86c89064ce4e4c82390f12a726771fb0ac8aa
kensho-pilkey <kpilkey@unc.edu>
2024-10-31 17:47:44 -0400

    Create random card generator, currently gives 3 cards off rip

--- Scripts/Global.cs
+++ Scripts/Global.cs
@@ -6,37 +6,53 @@ public partial class Global : Node
 {
 	public int PlayerScore { get; set; } = 0;
 	public List<CardData> PlayerCards { get; private set; } = new List<CardData>();
+	private static readonly Dictionary<string, string> NameAssetPairs = new Dictionary<string, string>
+    {
+        { "Dragon", "res://assets/dragon.png" },
+        { "Phoenix", "res://assets/phoenix.png" },
+        { "Griffin", "res://assets/griffin.png" },
+        { "Unicorn", "res://assets/unicorn.png" },
+        { "Hydra", "res://assets/hydra.png" }
+    };
+	private static readonly string[] Elements = { "Fire", "Water", "Grass" };
+	private static readonly Random RandomGenerator = new Random();
 	public override void _Ready()
 	{
-		PlayerCards.Add(new CardData("Dragon", "res://assets/dragon.png", 10, 10, "Fire"));
-		//above is example of adding card to player deck.
-		//TODO create randomizer to generate a random card.
-		Timer timer = new Timer();
-        timer.WaitTime = 1.0f; // Set the timer to fire every 1 second
-        timer.Autostart = true; // Start automatically when added to the scene
-        timer.Connect("timeout", new Callable(this, nameof(OnTimerTimeout))); // Connect timeout signal
-        AddChild(timer); // Add timer to the node tree
-
-        // Start the timer
-        timer.Start();
-
+		PlayerCards.Add(createRandomCard());
+		PlayerCards.Add(createRandomCard());
+		PlayerCards.Add(createRandomCard());
+		
+		foreach (var card in PlayerCards)
+			{
+				GD.Print(card.ToString());
+			}
 		
 	}
-	private void OnTimerTimeout()
+	public CardData createRandomCard()
     {
-        // Check if there is at least one card in the player's deck
-        if (PlayerCards.Count > 0)
-        {
-            // Print the details of the first card in PlayerCards (in this case, "Dragon")
-            GD.Print(PlayerCards[0].ToString());
-        }
+        // Select a random name-asset pair
+        var nameAsset = GetRandomNameAsset();
+
+        // Randomly select element type
+        string element = Elements[RandomGenerator.Next(Elements.Length)];
+
+        // Randomly assign damage (1-8) and health (3-10)
+        int damage = RandomGenerator.Next(1, 9); // Inclusive of 1-8
+        int health = RandomGenerator.Next(3, 11); // Inclusive of 3-10
+
+        return new CardData(nameAsset.Key, nameAsset.Value, damage, health, element);
     }
-	public CardData createRandomCard() {
-		Random random = new Random();
-		
-		return new CardData("Dragon", "res://assets/dragon.png", 10, 10, "Fire");
-	}
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
+
+	private KeyValuePair<string, string> GetRandomNameAsset()
+    {
+        // Convert dictionary keys to a list to access randomly
+        List<string> keys = new List<string>(NameAssetPairs.Keys);
+        string randomKey = keys[RandomGenerator.Next(keys.Count)];
+
+        return new KeyValuePair<string, string>(randomKey, NameAssetPairs[randomKey]);
+    }
+
+
 	public override void _Process(double delta)
 	{
 	}
