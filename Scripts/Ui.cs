98e621a84225fbfee9bc0f7165fdbea09468cc0a
kensho-pilkey <kpilkey@unc.edu>
2024-11-01 23:04:23 -0400

    Fix

--- Scripts/Ui.cs
+++ Scripts/Ui.cs
@@ -3,16 +3,10 @@ using System;
 
 public partial class Ui : Control
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
 	private void _on_deck_pressed(){
 		GetNode<Control>("Deck").Visible = true;
+	}
+	private void _on_shop_pressed(){
+		GetNode<Control>("Shop").Visible = true;
 	}
 }
